7db29a4d6aeb40846af197d9ada0c62ba0fb8c5d
dinhnamitvn <dinhnamitvn@gmail.com>
2016-01-15 15:15:30 +0700

    1

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,30 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+ 
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+      console.log(text);
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
   });
 }
